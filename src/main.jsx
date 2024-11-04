388970682dd1b5c856c726a9d7498138b00c094f
Duda-ifms <et49181@gmail.com>
2024-11-03 21:36:37 -0300

    atividade dia 03/11

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import styles from './Home.module.css';
+
+  const Home = () => {
+    return(
+      <div className={styles.home}>
+        <h1>Ola sou a Duda</h1>
+        <p>Esse codigo ta dando erro</p>
+      </div>
+
+    );
+  };
+     export default Home;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
